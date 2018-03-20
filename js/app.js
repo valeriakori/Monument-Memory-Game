89b5b2a4d102b50580a8169070d4716e1b444718
valeriakori <33432693+valeriakori@users.noreply.github.com>
2018-03-21 00:04:40 +0100

    Add timer function

--- js/app.js
+++ js/app.js
@@ -90,8 +90,14 @@ var selectedCardTwo = ""; //Second selected card, will be declared after click o
 
 var moves = document.getElementsByClassName("moves");
 var numberOfMoves = 0; //Initial value of moves
-var repeat = document.getElementsByClassName("restart");
+
+var restart = document.getElementsByClassName("restart");
+
 var stars = document.getElementsByClassName("stars");
+
+var minutesLabel = document.getElementById("minutes");
+var secondsLabel = document.getElementById("seconds");
+var totalSeconds = 0;
 
 /******************************************* 
 ** Selector of modal and respective spans **
@@ -106,11 +112,10 @@ var timeModal = document.getElementsByClassName("time-modal");
 ** Functions that initialize values and start game **
 *****************************************************/
 
-(function gameInit(){
-    moves.innerText = numberOfMoves;
-    console.log(moves.innerText);
-    shuffle(cards);
-})();
+// (function gameInit(){
+//     moves.innerText = numberOfMoves;
+//     console.log(moves.innerText);
+// })();
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -126,6 +131,10 @@ function shuffle(array) {
 
     return array;
 }
+shuffle(cards);
+
+
+//restart.addEventListener("click", gameInit);
 
 //Append values from array to ul
 for (i = 0 ; i < card.length; i++) {
@@ -137,6 +146,7 @@ for (i = 0 ; i < card.length; i++) {
 deck.addEventListener("click", function openCard(e){
 
     e.preventDefault();
+    startTimer(); //Starts timer TODO perform this action only once!
 
     if (e.target.hasAttribute("data-card-value")) {
 
@@ -209,9 +219,11 @@ deck.addEventListener("click", function openCard(e){
 });
 
 function changeScore() {
+    console.log(moves.innerHTML);
+
     numberOfMoves++;
-    moves.innerText = numberOfMoves;
-    console.log(moves.innerText);
+    moves.innerHTML = numberOfMoves;
+    //console.log(moves.innerHTML);
 
 
     if (numberOfMoves == 12) {
@@ -220,6 +232,36 @@ function changeScore() {
         //remove second star
     }
 }
+
+// Closure Function from https://stackoverflow.com/questions/12713564/function-in-javascript-that-can-be-called-only-once
+// Closure makes it that the function setTime(); can only be called once
+// Timer Functions from https://stackoverflow.com/questions/5517597/plain-count-up-timer-in-javascript
+
+var startTimer = (function() {
+    var executed = false;
+    return function() {
+        if (!executed) {
+            executed = true;
+            setInterval(setTime, 1000);
+            function setTime() {
+                ++totalSeconds;
+                secondsLabel.innerHTML = pad(totalSeconds % 60);
+                minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+              }
+              
+              function pad(val) {
+                var valString = val + "";
+                if (valString.length < 2) {
+                  return "0" + valString;
+                } else {
+                  return valString;
+                }
+              }
+        }
+    };
+})();
+
+
 
 function showModal() {
 }
